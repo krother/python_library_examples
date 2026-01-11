56367c162adf0c2eed19843d02f23064ccd8b600
Kristian Rother <kristian.rother@posteo.de>
2026-01-11 13:26:58 +0100

    change sphinx theme

--- conf.py
+++ conf.py
@@ -10,6 +10,7 @@ project = 'Python Library Examples'
 copyright = '2025, Kristian Rother'
 author = 'Kristian Rother'
 release = '1.0'
+html_title = f"{project}"
 
 # -- General configuration ---------------------------------------------------
 
@@ -18,33 +19,40 @@ extensions = [
     'sphinx_copybutton',
     'sphinx.ext.todo',
     'myst_parser',
+    #'sphinxcontrib.cairosvgconverter',
     ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'learning_goals.md', 'README.md']
 
-language = 'ls'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_theme_path = ['themes']
+html_theme = 'furo'
 html_static_path = ['_static']
-#html_logo = "_static/banner_wide.svg"
+html_logo = "_static/academis_logo.png"
 html_favicon = "_static/favicon.ico"
 
-html_sidebars = {
-    '**': [
-        'about.html',
-        'localtoc.html',
-        'searchbox.html',
-    ]
-}
+
+html_css_files = [
+    "academis.css",
+]
+
 html_theme_options = {
     'logo': 'academis.png',
     'github_user': 'krother',
     'github_repo': 'python_library_examples',
     'show_relbar_top' : True,
     'show_relbar_bottom' : True,
+    "source_repository": "https://github.com/krother/python_library_examples",
+    "source_branch": "main",
+    "source_directory": "",
+}
+
+# ---- Options for PDF output ----
+
+latex_elements = {
+    'preamble': r"\linespread{1.25}",
 }
